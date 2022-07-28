7f405bfba844c93f37e4417c2318d757ba29d237
Or Mizrahi <or.mizrahi1990@gmail.com>
2022-07-28 15:27:19 +0300

    Added chunks and Promise.all for parallelism

--- close-alerts/index.js
+++ close-alerts/index.js
@@ -3,6 +3,7 @@ const piDB = new PiDB(process.env.PI_REST_URL);
 const inquirer = require('inquirer');
 const inputDataGetter = require('./../common/inputDataGetter');
 const _ = require('lodash');
+const CHUNK_TO_UPDATE_SIZE = parseInt(process.env.CHUNK_TO_UPDATE_SIZE);
 
 const handler = async(actionContext) => {
     const inputData = await inputDataGetter.getInputData(false);
@@ -12,6 +13,7 @@ const handler = async(actionContext) => {
         { type: 'input', name: 'alert-message', message: 'What type of alert would you like to close? (free text)'},
       ]);
     const alertMessage = answer['alert-message'];
+    const allAlerts = [];
     for(const userId of userIds){
         try{
             const userAlerts = await alerts.getAlertsByMessage(userId, alertMessage);
@@ -19,11 +21,23 @@ const handler = async(actionContext) => {
                 console.log(`Could not find alerts for user ${userId}`);
                 continue;
             }
-            for(const userAlert of userAlerts){
-                await alerts.setDone(userId, userAlert.id, true, actionContext);
-                console.log(`Closed alert with the message: ${userAlert.message} for ${userId}`)
-            }
+            allAlerts.push(...userAlerts);
         } catch(error){
+            console.error(error);
+        }
+    }
+    console.log(`Total of ${allAlerts.length} alerts from ${userIds.length} are going to close`);
+    const chunks = _.chunk(allAlerts, CHUNK_TO_UPDATE_SIZE);
+    let alertsClosed = 0;
+    for(const chunk of chunks){
+        try{
+            await Promise.all(_.map(chunk, async(alert)=>{
+                await alerts.setDone(alert.user_id, alert.id, true, actionContext);
+                console.log(`Closed alert with the message: ${alert.message} for ${alert.user_id}`);
+            }));
+            alertsClosed += CHUNK_TO_UPDATE_SIZE;
+            console.log(`Closed ${alertsClosed} of ${allAlerts.length} alerts`);
+        } catch (error){
             console.error(error);
         }
     }
